1da00532d06b1abda8b3462b4757cbaf626a83c7
MetalGearSVT <metalgear753@gmail.com>
2021-11-13 17:30:10 -0500

    Setup Scripts

---
+++ util/README.txt
@@ -0,0 +1,1 @@
+put conf.py and initializeDatabase.py next to it
---
+++ util/installUser.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "Input username: "
+read user
+
+mkdir /home/ubuntu/markov/$user
+cp ../conf.py /home/ubuntu/markov/$user
+cp ../initializeDatabase.py /home/ubuntu/markov/$user
+
+
+echo "[Unit]
+Description=$user's Markov Bot
+After=multi-user.target
+
+[Service]
+Type=simple
+WorkingDirectory=/home/ubuntu/markov/$user/
+ExecStart=/usr/bin/python3 /home/ubuntu/markov/$user/markovChain.py
+StandardInput=tty-force
+User=ubuntu
+Restart=always
+
+[Install]
+WantedBy=multi-user.target" > /lib/systemd/system/$user.service;
+
+systemctl enable $user.service;
+systemctl start $user.service;
